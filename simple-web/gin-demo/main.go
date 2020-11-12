da3b97c7ec4745dd9b15b359d93d6b2a944e4339
latesun.lee <latesun.lee@gmail.com>
2020-11-12 16:22:44 +0800

    add a simple web server for test

---
+++ simple-web/gin-demo/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "")
+	})
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.Run()
+}
